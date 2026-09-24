735e2fa52fb9867034469cf1516a8b0bea71e8cc
yrz777 <sfeng750@connect.hkust-gz.edu.cn>
2026-09-24 21:09:54 +0800

    feat: 完成呼吸灯的CubeMX配置

--- stm32f407vgt6/Core/Src/main.c
+++ stm32f407vgt6/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "tim.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -86,6 +87,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
